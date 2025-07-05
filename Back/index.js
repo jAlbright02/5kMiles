d9110e2c60a79f93915b71abadecca35af914d80
James Albright <jamesdalbright02@gmail.com>
2025-07-05 14:34:23 +0100

    fix for frontend

--- Back/index.js
+++ Back/index.js
@@ -7,9 +7,9 @@ const PORT = 5001;
 app.use(cors());
 
 app.get('/api/message', (req, res) => {
-  res.json({ message: 'Hello from backend!' });
+  res.json({ message: 'Hello Patrick, it is me your father please send money paypal.me/jimjamflimflam' });
 });
 
 app.listen(PORT, () => {
-  console.log(`Backend listening on http://localhost:${PORT}`);
+  console.log(`Backend listening on http://myownwebsite:${PORT}`);
 });
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # 5kMiles
 Project to help manage your motorbike maintenance
+
+# Icons
+Icons used in this project are from [svgrepo](https://www.svgrepo.com)
