6f6d1b2a271483fbbbcff78ca327eb07bc5f12d0
James Albright <jamesdalbright02@gmail.com>
2025-07-05 14:52:51 +0100

    remove cors point to backend

--- Back/index.js
+++ Back/index.js
@@ -1,10 +1,7 @@
 import express from 'express';
-import cors    from 'cors';
 
 const app = express();
 const PORT = 5001;
-
-app.use(cors());
 
 app.get('/api/message', (req, res) => {
   res.json({ message: 'Hello Patrick, it is me your father please send money paypal.me/jimjamflimflam' });
--- Front/src/App.tsx
+++ Front/src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   const [message, setMessage] = useState('');
 
   useEffect(() => {
-    fetch('http://5kMilesBackend:5001/api/message')
+    fetch('5kmilesbackend/api/message')
       .then((res) => res.json())
       .then((data) => setMessage(data.message))
       .catch(console.error);
