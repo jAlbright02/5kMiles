e46af8b945e2736e71b6e8767ad41350af86f774
James Albright <jamesdalbright02@gmail.com>
2025-07-05 16:08:09 +0100

    revert back to url

--- Front/vite.config.ts
+++ Front/vite.config.ts
@@ -6,11 +6,5 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5000,
-    proxy: {
-      '/api': {
-        target: 'http://5kmilesbackend:5001',
-        changeOrigin: true,
-      },
-    },
   },
 })
