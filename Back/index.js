87651a84afc98d1ae2c37f750afaa952ef58e70a
James Albright <jamesdalbright02@gmail.com>
2025-07-05 16:48:39 +0100

    use cors

--- Back/index.js
+++ Back/index.js
@@ -1,7 +1,10 @@
 import express from 'express';
+import cors    from 'cors';
 
 const app = express();
 const PORT = 5001;
+
+app.use(cors());
 
 app.get('/api/message', (req, res) => {
   res.json({ message: 'Hello darling dearest, i hope this finds you well xx' });
