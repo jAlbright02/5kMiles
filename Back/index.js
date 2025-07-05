756e0ecfc875a38a7c6535523e6d3d3e934b0e80
James Albright <jamesdalbright02@gmail.com>
2025-07-05 16:36:59 +0100

    for cat

--- Back/index.js
+++ Back/index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 5001;
 
 app.get('/api/message', (req, res) => {
-  res.json({ message: 'Hello Patrick, it is me your father please send money paypal.me/jimjamflimflam' });
+  res.json({ message: 'Hello darling dearest, i hope this finds you well xx' });
 });
 
 app.listen(PORT, '0.0.0.0', () => {
--- Front/src/App.tsx
+++ Front/src/App.tsx
@@ -12,8 +12,11 @@ function App() {
 
   return (
     <div style={{ padding: 20 }}>
-      <h1>Message from backend:</h1>
+      <h1>Message from James:</h1>
       <p>{message || 'Loading...'}</p>
+      <p>Not sure how these will look btw</p>
+      <img src='https://i.pinimg.com/736x/e5/d1/f5/e5d1f519da3abe6631fed6850e73b924.jpg'></img>
+      <img src='https://media1.tenor.com/m/PDiYbMpkf-8AAAAd/dog-smile.gif'></img>
     </div>
   );
 }
