cdcff1793f814b3ac54a2be55f2840d1d10c4a88
James Albright <jamesdalbright02@gmail.com>
2025-07-05 14:57:00 +0100

    fix

--- Front/src/App.tsx
+++ Front/src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   const [message, setMessage] = useState('');
 
   useEffect(() => {
-    fetch('5kmilesbackend/api/message')
+    fetch('/api/message')
       .then((res) => res.json())
       .then((data) => setMessage(data.message))
       .catch(console.error);
