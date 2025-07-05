6750d5ef48ceabf2587dbd347c80aa7a5cad9d1d
James Albright <jamesdalbright02@gmail.com>
2025-07-05 15:47:37 +0100

    correct listener

--- Back/index.js
+++ Back/index.js
@@ -7,6 +7,6 @@ app.get('/api/message', (req, res) => {
   res.json({ message: 'Hello Patrick, it is me your father please send money paypal.me/jimjamflimflam' });
 });
 
-app.listen(PORT, () => {
-  console.log(`Backend listening on http://myownwebsite:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Backend listening on http://0.0.0.0:${PORT}`);
 });
